8df0e1c0a6a9f28547eae48162724cf7f021acba
Nicolas Meyer <impnicolasmeyer@gmail.com>
2023-01-30 14:54:49 -0300

    first commit

---
+++ Proyecto final integrador/Face2_.js
@@ -0,0 +1,10 @@
+
+
+document.getElementById ('boton_color').addEventListener ('click', function(){
+    document.body.style.backgroundColor = '#5A3434';
+});
+
+document.getElementById ('boton_default').addEventListener ('click', function(){
+    document.body.style.backgroundColor = 'white';
+});
+
